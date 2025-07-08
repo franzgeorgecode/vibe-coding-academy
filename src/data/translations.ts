259656785acc6085c7d2a6815669cf53bb304203
Vibe Coding Academy <noreply@vibecoding.academy>
2025-07-08 18:02:31 -0500

    Revert "Fix nextRelease undefined variable error in translations"
    This reverts commit d39630e7ffbfd4f59588bcc326c699d2095b05a9.

--- src/data/translations.ts
+++ src/data/translations.ts
@@ -15029,7 +15029,7 @@ module.exports = {
       '@semantic-release/git',
       {
         assets: ['CHANGELOG.md', 'package.json'],
-        message: 'chore(release): [VERSION] [skip ci]\\n\\n[RELEASE NOTES]'
+        message: 'chore(release): ${nextRelease.version} [skip ci]\\n\\n${nextRelease.notes}'
       }
     ]
   ]
